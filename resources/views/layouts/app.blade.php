3f4d325748ba1233b836c6eed317b471af318a87
Deny Pradia Utama <jr.pikong@gmail.com>
2018-04-18 14:55:09 +0700

    fix zendeks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,11 +35,12 @@
 
     <!--Start of Zendesk Chat Script-->
     <script type="text/javascript">
-        window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+        window.$zopim||(function(d,s){var z=$zopim=function(c){
+            z._.push(c)},$=z.s=
             d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-            .push(o)};z.=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
-            $.src="https://v2.zopim.com/?5dkkr8GWRMJkWEJYIKntEBHDi533Ejz8";z.t=+new Date;$.
-                type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
+        _.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+            $.src='https://v2.zopim.com/?5dkkr8GWRMJkWEJYIKntEBHDi533Ejz8';z.t=+new Date;$.
+                type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
     </script>
     <!--End of Zendesk Chat Script-->
 </head>
